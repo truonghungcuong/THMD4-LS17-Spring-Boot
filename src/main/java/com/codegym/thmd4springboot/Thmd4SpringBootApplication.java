712fb083ff8855d79893d183a9eab2460a3bb503
truonghungcuong <truonghungcuong12012001@gmail.com>
2022-04-07 10:17:13 +0700

    first commit

---
+++ src/main/java/com/codegym/thmd4springboot/Thmd4SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.thmd4springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thmd4SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Thmd4SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/thmd4springboot/Thmd4SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.thmd4springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thmd4SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
